c3619d5503d883142efc2636acec1a1d82435f15
gaby massabni <gabymassabni@gmail.com>
2024-04-11 16:37:43 +0300

    3rd commit deep linking

--- App.tsx
+++ App.tsx
@@ -1,12 +1,59 @@
-import React from 'react';
+// import React from 'react';
+// import {Provider} from 'react-redux';
+// import store from './src/redux/store';
+// import MainNavigation from './src/navigation/MainNavigation';
+
+// const App = () => (
+//   <Provider store={store}>
+//     <MainNavigation />
+//   </Provider>
+// );
+
+// export default App;
+
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
+import notifee, {EventType} from '@notifee/react-native';
 import store from './src/redux/store';
 import MainNavigation from './src/navigation/MainNavigation';
 
-const App = () => (
-  <Provider store={store}>
-    <MainNavigation />
-  </Provider>
-);
+function App(): React.JSX.Element {
+  useEffect(() => {
+    const unsubscribeForeground = notifee.onForegroundEvent(
+      async ({type, detail}) => {
+        await handleNotificationEvent(type, detail);
+      },
+    );
+
+    const unsubscribeBackground = notifee.onBackgroundEvent(
+      async ({type, detail}) => {
+        await handleNotificationEvent(type, detail);
+      },
+    );
+
+    return () => {};
+  }, []);
+
+  async function handleNotificationEvent(
+    type: EventType,
+    detail: any,
+  ): Promise<void> {
+    switch (type) {
+      case EventType.DISMISSED:
+        console.log('User dismissed notification', detail.notification);
+        break;
+      case EventType.PRESS:
+        console.log('User pressed notification', detail.notification);
+        console.log('User pressed notification', detail.notification);
+        break;
+    }
+  }
+
+  return (
+    <Provider store={store}>
+      <MainNavigation />
+    </Provider>
+  );
+}
 
 export default App;
